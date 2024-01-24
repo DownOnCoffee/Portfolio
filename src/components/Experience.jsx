9e5a556ee998d5b938fb5afe6a4b0d00ffa8b777
Manya Raghuvanshi <manyaraghuvanshi66@gmail.com>
2024-01-24 16:50:10 +0530

    fixed animation issue in exp tab

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,10 +7,40 @@ import { styles } from "../styles";
 import { useInView } from "react-intersection-observer";
 import { easeInOut, easeOut } from "framer-motion";
 const Experience = () => {
+
   const isMobile = window.innerWidth <= 768;
-  const [ref, inView] = useInView({
-    triggerOnce: true, // Ensures the animation runs once when the element enters the viewport
+  const [ref1, inView1] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
   });
+
+  const [ref2, inView2] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+
+  const [ref3, inView3] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+
+  const [ref4, inView4] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+  const [ref, inView] = useInView({
+    
+    triggerOnce: true, 
+    threshold:"0.8"
+    // Ensures the animation runs once when the element enters the viewport
+  });
+  // useEffect(() => {
+  //   const timeout = setTimeout(() => {
+  //     // Trigger animations or set state for animations here
+  //   }, 1000); // Adjust the delay as needed
+  
+  //   return () => clearTimeout(timeout);
+  // }, [inView]);
 
   const expcardstyleright =
     "flex-shrink-0 border-b-4 border-r-4 bg-gray-900 p-7 w-[400px]  rounded-xl leading-6";
@@ -21,6 +51,8 @@ const Experience = () => {
     const projlistitems2=["Developed interfaces for multiple projects","Contributed to the development of a dynamic land rental website - Patched and accumulated industrial experience "];
     const projlistitems3=["Engaged in an online frontend developer internship at Collcom ","Collaborated with a non-profit organization to develop a website for their Cybersecurity awareness campaign."];
     const projlistitems4=["Completed comprehensive training in HTML, CSS, and JavaScript by completing certifications."," acquiring essential skills for developing modern and responsive websites."];
+
+
     
 
   return (
@@ -40,19 +72,19 @@ const Experience = () => {
               <motion.div
                 className="font-italic whitespace-nowrap "
                 whileInView={{
-                  x: inView ? -250 : -150,
-                  opacity: inView ? 1 : 0,
+                  x: inView1 ? -250 : -180,
+                  opacity: inView1 ? 1 : 0,
                 }}
-                transition={{ delay: 0.3, duration: 0.6 }}
-                ref={ref}
-                initial={{ x: -150, opacity: 0 }}
+                transition={{ delay: inView1 ? 0.3 : 0, duration: 0.6 }}
+                ref={ref1}
+                initial={{ x: -180, opacity: 0 }}
               >
                 August'23 - December'23
               </motion.div>
               <motion.div
                 className={expcardstyleright}
-                ref={ref}
-                whileInView={{ x: inView ? -160 : 0 }}
+                ref={ref1}
+                whileInView={{ x: inView1 ? -160 : 0 }}
                 initial={{ x: 0 }}
                 transition={{ delay: 0.1, duration: 0.9 }}
               >
@@ -73,19 +105,19 @@ const Experience = () => {
               <motion.div
                 className="font-italic whitespace-nowrap"
                 whileInView={{
-                  x: inView ? 50 : 0,
-                  opacity: inView ? 1 : 0,
+                  x: inView2 ? 50 : 0,
+                  opacity: inView2 ? 1 : 0,
                 }}
                 transition={{ delay: 0.3, duration: 0.6 }}
-                ref={ref}
+                ref={ref2}
                 initial={{ x: 0, opacity: 0 }}
               >
                 June'23 - August'23
               </motion.div>
               <motion.div
                 className={expcardstyleleft}
-                ref={ref}
-                whileInView={{ x: inView ? -580 : -700 }}
+                ref={ref2}
+                whileInView={{ x: inView2 ? -580 : -700 }}
                 initial={{ x: -700 }}
                 transition={{ delay: 0.1, duration: 0.9 }}
               >
@@ -107,19 +139,19 @@ const Experience = () => {
               <motion.div
                 className="font-italic whitespace-nowrap "
                 whileInView={{
-                  x: inView ? -180 : -150,
-                  opacity: inView ? 1 : 0,
+                  x: inView3 ? -180 : -150,
+                  opacity: inView3 ? 1 : 0,
                 }}
                 transition={{ delay: 0.3, duration: 0.6 }}
-                ref={ref}
+                ref={ref3}
                 initial={{ x: -150, opacity: 0 }}
               >
                 April'23 - June'23
               </motion.div>
               <motion.div
                 className={expcardstyleright}
-                ref={ref}
-                whileInView={{ x: inView ? -100 : 0 }}
+                ref={ref3}
+                whileInView={{ x: inView3 ? -100 : 0 }}
                 initial={{ x: 0 }}
                 transition={{ delay: 0.1, duration: 0.9 }}
               >
@@ -140,19 +172,19 @@ const Experience = () => {
               <motion.div
                 className="font-italic whitespace-nowrap"
                 whileInView={{
-                  x: inView ? 50 : 0,
-                  opacity: inView ? 1 : 0,
+                  x: inView4 ? 50 : 0,
+                  opacity: inView4 ? 1 : 0,
                 }}
                 transition={{ delay: 0.3, duration: 0.6 }}
-                ref={ref}
+                ref={ref4}
                 initial={{ x: 0, opacity: 0 }}
               >
                 2022 - 2023
               </motion.div>
               <motion.div
                 className={expcardstyleleft}
-                ref={ref}
-                whileInView={{ x: inView ? -530 : -700 }}
+                ref={ref4}
+                whileInView={{ x: inView4 ? -530 : -700 }}
                 initial={{ x: -700 }}
                 transition={{ delay: 0.1, duration: 0.9 }}
               >
